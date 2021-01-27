44d9eccc9e950ae79d1884d95122c072e8772e9e
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-27 23:32:28 +0700

    update login

---
+++ pages/admin/login/.index.jsx
@@ -0,0 +1,60 @@
+import styles from "../../../styles/login.module.css";
+import React, { useState } from "react";
+import LayoutAdmin from "../../../components/admin/LayoutAdmin";
+
+const index = () => {
+  //show password
+  const [show, setShow] = useState(false);
+  const handleLogin = (e) => {
+    const username = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+    e.preventDefault();
+    const input_username = e.target.username.value;
+    const input_password = e.target.password.value;
+    if (input_username === username && input_password === password) {
+      alert("login berhasil");
+    } else {
+      alert("periksa kembali account anda..");
+    }
+  };
+  return (
+    <>
+      <div className={styles.container}>
+        <form className={styles.login_form} onSubmit={handleLogin}>
+          <h3>Admin Dashboard Login</h3>
+          <div className={styles.form_group}>
+            <label htmlFor="username">username</label>
+            <input type="text" name="username" id="username" />
+          </div>
+
+          <div className={styles.form_group}>
+            <label htmlFor="password">password</label>
+            <input
+              type={show ? "text" : "password"}
+              name="password"
+              id="password"
+            />
+            <div className={styles.form_group_select}>
+              <input
+                type="checkbox"
+                name="show_password"
+                id="show_password"
+                onClick={() => {
+                  setShow(!show);
+                }}
+              ></input>
+              <label htmlFor="show_password">
+                <small>show password</small>
+              </label>
+            </div>
+            <div className={styles.form_group}>
+              <button type="submit">login</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default index;
--- pages/admin/login/index.jsx
+++ pages/admin/login/index.jsx
@@ -1,60 +1,70 @@
-import styles from "../../../styles/login.module.css";
-import React, { useState } from "react";
-import LayoutAdmin from "../../../components/admin/LayoutAdmin";
+import { useState } from "react";
+import styles from "../../../styles/Login.module.css";
 
-const index = () => {
-  //show password
+const Login = () => {
   const [show, setShow] = useState(false);
+
   const handleLogin = (e) => {
-    const username = process.env.USERNAME;
-    const password = process.env.PASSWORD;
     e.preventDefault();
     const input_username = e.target.username.value;
     const input_password = e.target.password.value;
-    if (input_username === username && input_password === password) {
-      alert("login berhasil");
-    } else {
-      alert("periksa kembali account anda..");
-    }
+    // alert(input_username + " " + input_password);
+    fetch("http://35.186.154.72:9000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: input_username,
+        password: input_password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.kode === 200) {
+          alert(data.pesan);
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("username", data.data.username);
+        } else {
+          alert(data.pesan);
+        }
+      });
   };
   return (
-    <LayoutAdmin>
-      <div className={styles.container}>
-        <form className={styles.login_form} onSubmit={handleLogin}>
-          <h3>Admin Dashboard Login</h3>
-          <div className={styles.form_group}>
-            <label htmlFor="username">username</label>
-            <input type="text" name="username" id="username" />
-          </div>
+    <main className={styles.container}>
+      <form className={styles.login_form} onSubmit={handleLogin}>
+        <div className={styles.form_group}>
+          <label htmlFor="username">username</label>
+          <input type="text" id="username" name="username" required />
+        </div>
 
-          <div className={styles.form_group}>
-            <label htmlFor="password">password</label>
-            <input
-              type={show ? "text" : "password"}
-              name="password"
-              id="password"
-            />
-            <div className={styles.form_group_select}>
-              <input
-                type="checkbox"
-                name="show_password"
-                id="show_password"
-                onClick={() => {
-                  setShow(!show);
-                }}
-              ></input>
-              <label htmlFor="show_password">
-                <small>show password</small>
-              </label>
-            </div>
-            <div className={styles.form_group}>
-              <button type="submit">login</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </LayoutAdmin>
+        <div className={styles.form_group}>
+          <label htmlFor="password">password</label>
+          <input
+            type={show ? "text" : "password"}
+            id="password"
+            name="password"
+            required
+          />
+        </div>
+
+        <div className={styles.show_password}>
+          <input
+            type="checkbox"
+            id="show_password"
+            onChange={() => {
+              setShow(!show);
+            }}
+          />
+          <label htmlFor="show_password">tampilkan password</label>
+        </div>
+
+        <div className={styles.form_group}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </main>
   );
 };
 
-export default index;
+export default Login;
